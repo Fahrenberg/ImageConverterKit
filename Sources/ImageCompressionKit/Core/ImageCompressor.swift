293d3d800c795e51a92c049836ebc91333fa908d
Jean-Nicolas <70505484+Fahrenberg@users.noreply.github.com>
2026-07-07 16:47:49 +0200

    refactor: Updated and added image converted for png, heic, jpeg
    added png converter

--- Sources/ImageCompressionKit/Core/ImageCompressor.swift
+++ Sources/ImageCompressionKit/Core/ImageCompressor.swift
@@ -15,51 +15,68 @@ public enum ImageCompressor {
 
     public static func heicData(
         from imageData: Data,
-        compressionQuality: CGFloat = 0.75
+        compressionQuality: Double = 0.75
     ) -> Data? {
-        compressedData(
+        // HEIC specific, must be less than 1
+        guard compressionQuality < 1 else { return nil }
+        guard let heicData = convertData(
             from: imageData,
             type: AVFileType.heic as CFString,
             compressionQuality: compressionQuality
-        )
+            )
+        else { return nil}
+        return heicData.count <= imageData.count ? heicData : nil
     }
 
     public static func jpegData(
         from imageData: Data,
-        compressionQuality: CGFloat = 0.65
+        compressionQuality: Double = 0.65
     ) -> Data? {
-        compressedData(
+        guard let jpegData = convertData(
             from: imageData,
             type: UTType.jpeg.identifier as CFString,
             compressionQuality: compressionQuality
-        )
+            )
+        else { return nil }
+        return jpegData.count <= imageData.count ? jpegData : nil
     }
 
-    private static func compressedData(
-           from imageData: Data,
-           type: CFString,
-           compressionQuality: CGFloat
-       ) -> Data? {
-           let quality = min(max(compressionQuality, 0), 1)
-           guard let source = CGImageSourceCreateWithData(imageData as CFData, nil),
-                 CGImageSourceGetCount(source) > 0,
-                 let cgImage = CGImageSourceCreateImageAtIndex(source, 0, nil)
-           else {
-               return nil
-           }
-           let output = NSMutableData()
-           guard let destination = CGImageDestinationCreateWithData(output,type, 1, nil)
-           else {
-               return nil
-           }
+    public static func pngData(from imageData: Data) -> Data? {
+       let pngData =  convertData(
+            from: imageData,
+            type: UTType.png.identifier as CFString
+        )
+        return pngData
+    }
 
-           let options: NSDictionary = [
-               kCGImageDestinationLossyCompressionQuality: quality
-           ]
-           CGImageDestinationAddImage(destination, cgImage, options)
-           guard CGImageDestinationFinalize(destination) else {
-               return nil
-           }
-           return output as Data
-       }
+    private static func convertData(
+        from imageData: Data,
+        type: CFString,
+        compressionQuality: CGFloat? = nil
+    ) -> Data? {
+        let quality = compressionQuality.map { min(max($0, 0), 1) }
+        
+        guard let source = CGImageSourceCreateWithData(imageData as CFData, nil),
+              CGImageSourceGetCount(source) > 0,
+              let cgImage = CGImageSourceCreateImageAtIndex(source, 0, nil)
+        else {
+            return nil
+        }
+        let output = NSMutableData()
+        guard let destination = CGImageDestinationCreateWithData(output,type, 1, nil)
+        else {
+            return nil
+        }
+
+        let options: NSDictionary? = quality.map {
+            [kCGImageDestinationLossyCompressionQuality: $0]
+        }
+
+        CGImageDestinationAddImage(destination, cgImage, options)
+        guard CGImageDestinationFinalize(destination) else {
+            return nil
+        }
+        
+        return output as Data
+    }
 }
