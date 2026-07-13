414444e174e99fe417a52e19627d2622843a626b
Jean-Nicolas <70505484+Fahrenberg@users.noreply.github.com>
2026-07-13 16:46:28 +0200

    test - ImageCompressionPerformanceTests: updated test
    jpg faster than heic
    disabled performance testing

--- Tests/ImageCompressionKitTests/PlatformImage+CompressionPerformanceTests.swift
+++ Tests/ImageCompressionKitTests/PlatformImage+CompressionPerformanceTests.swift
@@ -22,18 +22,17 @@ final class ImageCompressionPerformanceTests: XCTestCase {
         let jpgQuality: CGFloat = 0.1
         let image = try XCTUnwrap(TestImage.image(size: .large))
         
-        self.measure(options: measureOnlyOnce) { // 0.025
+        self.measure(options: measureOnlyOnce) { // 0.439 (2x), macmini m4 pro simulator
             _ = try? XCTUnwrap(image.jpgData(quality: jpgQuality))
         }
     }
-/*
     
     func testPerformanceHEICCompressorLargeData() throws {
         let heicQuality: CGFloat = 0.1
         let image = try XCTUnwrap(TestImage.image(size: .large))
         
-        self.measure(options: measureOnlyOnce) { // 0.240
-            _ = image.heicDataCompression(compressionQuality: heicQuality)
+        self.measure(options: measureOnlyOnce) { // 0.556 (2x)
+            _ = try? XCTUnwrap(image.heicData(quality: heicQuality))
         }
     }
     
@@ -43,12 +42,12 @@ final class ImageCompressionPerformanceTests: XCTestCase {
         
         let images = Array(repeating: image, count: 4)
         
-        self.measure(options: measureOnlyOnce) { // 1.552 sec
+        self.measure(options: measureOnlyOnce) { // 1.133 sec
             let expectation = XCTestExpectation(description: "Concurrent HEIC compression completed")
 
             Task {
-                await images.concurrentForEach { image in
-                    _ =  image.heicDataCompression(compressionQuality: heicQuality)
+                await images.concurrentForEach(withPriority: .background) { image in
+                    _ =  try? XCTUnwrap(image.heicData(quality: heicQuality))
                 }
                 expectation.fulfill()
             }
@@ -63,25 +62,25 @@ final class ImageCompressionPerformanceTests: XCTestCase {
         
         let images = Array(repeating: image, count: 4)
         
-        self.measure(options: measureOnlyOnce){ // 1.17 sec
+        self.measure(options: measureOnlyOnce){ // 2.13 sec
             images.forEach { image in
-                    _ =  image.heicDataCompression(compressionQuality: heicQuality)
+                _ =  try? XCTUnwrap(image.heicData(quality: heicQuality))
             }
         }
     }
 
     func testPerformance4JPGCompressionsConcurrent() async throws {
-        let heicQuality: CGFloat = 0.5
+        let jpgQuality: CGFloat = 0.5
         let image = try XCTUnwrap(TestImage.image(size: .large))
         
         let images = Array(repeating: image, count: 4)
         
-        self.measure(options: measureOnlyOnce) { // 0.104 sec
+        self.measure(options: measureOnlyOnce) { // 0.517 sec
             let expectation = XCTestExpectation(description: "Concurrent JPG compression completed")
 
             Task {
-                await images.concurrentForEach { image in
-                    _ = image.jpgDataCompression(compressionQuality: heicQuality)
+                await images.concurrentForEach(withPriority: .background) { image in
+                    _ = try? XCTUnwrap(image.jpgData(quality: jpgQuality))
                 }
                 expectation.fulfill()
             }
@@ -91,19 +90,16 @@ final class ImageCompressionPerformanceTests: XCTestCase {
     }
     
     func testPerformance4JPGCompressionsSync() async throws {
-        let heicQuality: CGFloat = 0.5
+        let jpgQuality: CGFloat = 0.5
         let image = try XCTUnwrap(TestImage.image(size: .large))
         
         let images = Array(repeating: image, count: 4)
         
-        self.measure { // 0.177 sec
+        self.measure { // 1.854 sec
              images.forEach { image in
-                    _ = image.jpgDataCompression(compressionQuality: heicQuality)
+                 _ = try? XCTUnwrap(image.jpgData(quality: jpgQuality))
             }
         }
     }
-*/
-    
-     
 }
 
