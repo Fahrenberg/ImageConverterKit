cc8edd8a136fc52a457b8689ca4ae6d63333168d
Jean-Nicolas <70505484+Fahrenberg@users.noreply.github.com>
2026-07-15 11:44:40 +0200

    refactor - ImageConverter: added Data extension to convert data into a CGImage?

--- Sources/ImageCompressionKit/Data/Data+ImageConverter.swift
+++ Sources/ImageCompressionKit/Data/Data+ImageConverter.swift
@@ -10,45 +10,56 @@ import Extensions
 import ImageIO
 import UniformTypeIdentifiers
 
-
+//MARK: HEIC
 extension Data {
     public func heicData(
         quality: Double = ImageConverter.defaultHEICQuality
     ) -> Data? {
         // HEIC specific, compressionQuality must be < 1, precondition stops runtime, assume programmer error
         precondition(
-                (0..<1).contains(quality),
-                "heic compression quality must be between 0 and lower than 1."
-            )
+            (0..<1).contains(quality),
+            "heic compression quality must be between 0 and lower than 1."
+        )
         guard quality < 1 else
         { return nil }
         guard let heicData = ImageConverter.convertData(
             from: self,
             type: .heic,
             quality: quality
-            )
+        )
         else { return nil}
         return heicData.count <= self.count ? heicData : nil
     }
-
     
+    public func heicData(
+        askedMaxSize: Int
+    ) -> Data? {
+        return nil
+    }
+}
+
+//MARK: JPEG
+extension Data {
     public  func jpegData(
         quality: Double = ImageConverter.defaultJPEGQuality
     ) -> Data? {
         // CompressionQuality must be <= 1, precondition stops runtime, assume programmer error
         precondition(
-                (0...1).contains(quality),
-                "jpegData compression quality must be between 0 and 1."
-            )
+            (0...1).contains(quality),
+            "jpegData compression quality must be between 0 and 1."
+        )
         guard let jpegData = ImageConverter.convertData(
             from: self,
             type: .jpeg,
             quality: quality
-            )
+        )
         else { return nil }
         return jpegData.count <= self.count ? jpegData : nil
     }
+}
 
+//MARK: PNG
+extension Data {
     public func pngData() -> Data? {
         let pngData =  ImageConverter.convertData(
             from: self,
@@ -58,4 +69,16 @@ extension Data {
     }
     
     
+}
+
+internal extension Data {
+    /// Adapter to return an CGImage for Data on all platforms
+    var platformCGImage: CGImage? {
+        guard let source = CGImageSourceCreateWithData(self as CFData, nil),
+              CGImageSourceGetCount(source) > 0,
+              let cgImage = CGImageSourceCreateImageAtIndex(source, 0, nil)
+        else { return nil }
+        
+        return cgImage
+    }
 }
